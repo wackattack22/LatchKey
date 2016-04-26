21d87848851b58b600c650ca6c37fe91620089dd
wackattack22 <leo.wack@gmail.com>
2016-04-26 08:25:28 -0400

    small update
    Added Bat/Arrow/Archie scripts, still needs work

---
+++ LatchKeyDevelopment/Assets/Scripts/ArchieController.cs
@@ -0,0 +1,193 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArchieController : MonoBehaviour
+{
+
+    private Vector2 moveDir;
+    private int moveChoice;
+
+    private float time;
+
+    private bool colliding;
+
+    private Animator boblinAnim;
+
+    private bool isWalking;
+
+    private Vector3 target;
+
+    private float lineOfSight;
+    private float moveSpeed;
+
+    private GameObject arrow;
+
+    private GameObject player;
+
+    private float shotTimer;
+
+
+    void Start()
+    {
+        arrow = (GameObject)Resources.Load("Arrow");
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        shotTimer = 0;
+
+        //Random direction
+
+        boblinAnim = GetComponent<Animator>();
+        moveChoice = Random.Range(-2, 2);
+
+        switch (moveChoice)
+        {
+            case -1:
+                moveDir = Vector2.down;
+                break;
+            case 1:
+                moveDir = Vector2.up;
+                break;
+            case 2:
+                moveDir = Vector2.right;
+                break;
+            case -2:
+                moveDir = Vector2.left;
+                break;
+            case 0:
+                moveChoice = Random.Range(-2, 2);
+                break;
+        }
+
+
+        //Random time to move in direction
+        time = Random.Range(1f, 3f);
+
+
+    }
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+        this.GetComponent<Rigidbody2D>().velocity = moveDir.normalized * 2f;
+        boblinAnim.SetInteger("moveChoice", moveChoice);
+
+        switch (moveChoice)
+        {
+            case -1:
+                moveDir = Vector2.down;
+                break;
+            case 1:
+                moveDir = Vector2.up;
+                break;
+            case 2:
+                moveDir = Vector2.right;
+                break;
+            case -2:
+                moveDir = Vector2.left;
+                break;
+            case 0:
+                moveChoice = Random.Range(-2, 2);
+                break;
+        }
+
+        if (colliding)    //Reverse direction
+        {
+            moveChoice *= -1;
+            colliding = false;
+        }
+
+        //Time is up, new random direction and interval
+        if (time <= 0)
+        {
+            moveChoice = Random.Range(-2, 2);
+
+            switch (moveChoice)
+            {
+                case -1:
+                    moveDir = Vector2.down;
+                    break;
+                case 1:
+                    moveDir = Vector2.up;
+                    break;
+                case 2:
+                    moveDir = Vector2.right;
+                    break;
+                case -2:
+                    moveDir = Vector2.left;
+                    break;
+                case 0:
+                    moveChoice = Random.Range(-2, 2);
+                    break;
+            }
+
+            time = Random.Range(1f, 3f);
+        }
+
+
+        if (Mathf.Floor(transform.localPosition.y) == Mathf.Floor(player.transform.localPosition.y) || Mathf.Floor(transform.localPosition.x) == Mathf.Floor(player.transform.localPosition.x))
+        {
+            Shoot();
+        }
+        else shotTimer = 0;
+    }
+
+ 
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //collisions that reverse direction
+        if (col.gameObject.layer == 8)  //wall
+        {
+            colliding = true;
+        }
+        else if (col.gameObject.layer == 9) //hazard
+        {
+            colliding = true;
+        }
+        else if (col.gameObject.tag == "Lava")
+        {
+            colliding = true;
+        }
+        else if (col.gameObject.layer == 13)    //enemy
+        {
+            colliding = true;
+        }
+        else if (col.gameObject.tag == "Switch")
+        {
+            colliding = true;
+        }
+
+        //collisions that kill boblin
+        if (col.gameObject.name == "projectile")
+        {
+            PlayerController.lvlScore += 10;
+            Kill();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.layer == 9) //hazard
+        {
+            colliding = true;
+        }
+    }
+
+    void Shoot()
+    {
+        shotTimer -= Time.deltaTime;
+        if (shotTimer < 0) { 
+            GameObject projectile = Instantiate(arrow) as GameObject;
+            projectile.name = "Arrow";
+            Physics2D.IgnoreCollision(projectile.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+            projectile.transform.position = transform.position;
+            shotTimer = 0.5f;
+        }
+    }
+
+    public void Kill()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ LatchKeyDevelopment/Assets/Scripts/BatController.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatController : MonoBehaviour {
+
+    private Quaternion defaultRotation;
+    private Vector3 defaultPosition;
+    private Vector3 target;
+    private Vector3 dir;
+
+    private float lineOfSight;
+    private float moveSpeed;
+    private float angle;
+    private const float startRange = 0.1f;
+
+    private GameObject player;
+    private PlayerController playerController;
+
+    // Use this for initialization
+    void Start () {
+        defaultPosition = transform.position;
+        defaultRotation = transform.rotation;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerController = player.GetComponent<PlayerController>();
+        //Range at which enemy will chase player
+        lineOfSight = 5;
+        //Enemy chase speed
+        moveSpeed = 2f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector2.Distance(transform.position, player.transform.position) < lineOfSight)  //Player in enemy range
+        {
+            target = player.transform.position;          
+            MoveTowards(target);
+        }
+        else
+        {           
+            if (Vector2.Distance(transform.position, defaultPosition) > startRange) //Player out of enemy range, not at defaultPosition
+            {
+                MoveTowards(defaultPosition);
+            }
+            else    //Resets object/sprite position and rotation
+            {
+                transform.rotation = defaultRotation;
+                transform.position = defaultPosition;
+            }
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "projectile")
+        {
+            PlayerController.lvlScore += 15;
+                Kill();
+        }
+        
+        /*Logic for when player is blocking?*/
+
+    }
+
+    //There's a much simpler way to do this using transform.LookAt(), but that rotates the z axis
+    //Enemy faces and moves toward a target position
+    void MoveTowards(Vector3 target)
+    {   
+        dir = target - transform.position;
+        angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.position += transform.right * moveSpeed * Time.deltaTime;
+    }
+
+    public void Kill()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
